10330057b8db85a3d0c6be0af14f24e8ace64f92
Tracy Gius <tracy.gius@emc.com>
2016-01-12 14:45:11 -0800

    testing echo secure

--- build_children.sh
+++ build_children.sh
@@ -16,8 +16,9 @@ BUILD_NUM_TFP=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-tftp/builds' 
 BUILD_NUM_TASKS=$(curl -s 'https://api.travis-ci.org/repos/RackHD/on-tasks/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
 
 
-echo BUILD_NUM_DHCP
-echo BUILD_NUM_HTTP
+echo $AUTH_TOKEN
+echo $BUILD_NUM_DHCP
+echo $BUILD_NUM_HTTP
 
 # Restart last child project build
 curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM_DHCP/restart --header "Authorization: token "$AUTH_TOKEN
